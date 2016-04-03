d4288956d4968aee702030034db96edb1be8ae2f
taglia_1@hotmai.it <taglia_1@hotmai.it>
2016-04-03 15:54:40 +0200

    spostati css, ecc

---
+++ public/footer.php
@@ -0,0 +1,24 @@
+
+		<!-- /. PAGE WRAPPER  -->
+	</div>
+	<!-- /. WRAPPER  -->
+
+	<div id="footer-sec">
+		&copy; 2014 YourCompany | Design By : <a
+			href="http://www.binarytheme.com/" target="_blank">BinaryTheme.com</a>
+	</div>
+	<!-- /. FOOTER  -->
+	<!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+	<!-- JQUERY SCRIPTS -->
+	<script src="js/jquery-1.10.2.js"></script>
+	<!-- BOOTSTRAP SCRIPTS -->
+	<script src="js/bootstrap.js"></script>
+	<!-- METISMENU SCRIPTS -->
+	<script src="js/jquery.metisMenu.js"></script>
+	<!-- CUSTOM SCRIPTS -->
+	<script src="js/custom.js"></script>
+
+
+
+</body>
+</html>
---
+++ public/leftPanel.php
@@ -0,0 +1,32 @@
+
+		
+		
+		
+		<!-- /. NAV TOP  -->
+		<nav class="navbar-default navbar-side" role="navigation">
+			<div class="sidebar-collapse">
+				<ul class="nav" id="main-menu">
+					<li>
+						<div class="user-img-div">
+							<!-- 
+ <img src="img/user.png" class="img-thumbnail" />
+ -->
+
+							<div class="inner-text">
+								Jhon Deo Alex <br /> <small>Last Login : 2 Weeks Ago </small>
+							</div>
+						</div>
+
+					</li>
+
+
+					<li><a class="active-menu" href="index.php"><i
+							class="fa fa-desktop "></i>Home</a></li>
+					<li><a href="archivio.php"><i class="fa fa-sign-in "></i>Archivio</a>
+					</li>
+				</ul>
+
+			</div>
+
+		</nav>
+		
