0c086706fb109b6e3eca4e14f75f02050d20cc49
taglia_1@hotmai.it <taglia_1@hotmai.it>
2016-04-17 13:34:51 +0200

    common.php

---
+++ public_html/languages/lang.en.php
@@ -0,0 +1,39 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+ 
+$lang = array();
+ 
+$lang['PAGE_TITLE'] = 'My website page title';
+$lang['HEADER_TITLE'] = 'My website header title';
+$lang['SITE_NAME'] = 'My Website';
+$lang['SLOGAN'] = 'My slogan here';
+$lang['HEADING'] = 'Heading';
+ 
+// Home
+ 
+$lang['HOME_PROVA'] = 'Insert something here';
+
+// Menu
+
+$lang['LEFT_MENU_HOME'] = 'Home';
+$lang['LEFT_MENU_ARCHIVIO'] = 'File';
+$lang['LEFT_MENU_LINGUA'] = 'Language';
+
+//Archivio
+
+$lang['ARCHIVIO_HEADLINE'] = 'File';
+$lang['ARCHIVIO_INSERIMENTO'] = 'Insert new file';
+$lang['ARCHIVIO_NOME_CLIENTE'] = 'Client name';
+$lang['ARCHIVIO_BANCA'] = 'Bank';
+$lang['ARCHIVIO_NUOVA_BANCA'] = 'New bank';
+$lang['ARCHIVIO_DESCRIZIONE'] = 'Description';
+$lang['ARCHIVIO_INSERISCI'] = 'Insert';
+$lang['ARCHIVIO_LISTA_ARCHIVIO'] = 'File list';
+
+
+
+?>
---
+++ public_html/languages/lang.it.php
@@ -0,0 +1,38 @@
+<?php
+/* 
+-----------------
+Language: Italian
+-----------------
+*/
+ 
+$lang = array();
+ 
+$lang['PAGE_TITLE'] = 'My website page title';
+$lang['HEADER_TITLE'] = 'My website header title';
+$lang['SITE_NAME'] = 'My Website';
+$lang['SLOGAN'] = 'My slogan here';
+$lang['HEADING'] = 'Heading';
+ 
+// Home
+ 
+$lang['HOME_PROVA'] = 'Inserisci qualcosa';
+
+// Menu
+
+$lang['LEFT_MENU_HOME'] = 'Home';
+$lang['LEFT_MENU_ARCHIVIO'] = 'Archivio';
+$lang['LEFT_MENU_LINGUA'] = 'Lingua';
+
+// Archivio
+
+$lang['ARCHIVIO_HEADLINE'] = 'Archivio';
+$lang['ARCHIVIO_INSERIMENTO'] = 'Inserimento archivio';
+$lang['ARCHIVIO_NOME_CLIENTE'] = 'Nome cliente';
+$lang['ARCHIVIO_BANCA'] = 'Banca';
+$lang['ARCHIVIO_NUOVA_BANCA'] = 'Nuova banca';
+$lang['ARCHIVIO_DESCRIZIONE'] = 'Descrizione';
+$lang['ARCHIVIO_INSERISCI'] = 'Inserisci';
+$lang['ARCHIVIO_LISTA_ARCHIVIO'] = 'Lista archivio';
+
+
+?>
